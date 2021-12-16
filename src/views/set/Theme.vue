22db96648540dd1902b894af9c8a3b41a5aab26b
zhangjing <2694713772@qq.com>
2021-12-16 14:25:57 +0800

    主题页面样式

--- src/views/set/Theme.vue
+++ src/views/set/Theme.vue
@@ -1,6 +1,229 @@
 <template>
-  <div>
-      Theme
+  <div class="warp">
+    <div class="palette-row">
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Dust Red / 薄暮</div>
+          <span class="description">斗志、奔放</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff1f0"><span class="main-color-text">red-1</span></div>
+          <div class="main-color-item" style="background-color:#ffccc7"><span class="main-color-text">red-2</span></div>
+          <div class="main-color-item" style="background-color:#ffa39e"><span class="main-color-text">red-3</span></div>
+          <div class="main-color-item" style="background-color:#ff7875"><span class="main-color-text">red-4</span></div>
+          <div class="main-color-item" style="background-color:#ff4d4f"><span class="main-color-text">red-5</span></div>
+          <div class="main-color-item" style="background-color:#f5222d"><span class="main-color-text">red-6</span></div>
+          <div class="main-color-item" style="background-color:#cf1322"><span class="main-color-text">red-7</span></div>
+          <div class="main-color-item" style="background-color:#a8071a"><span class="main-color-text">red-8</span></div>
+          <div class="main-color-item" style="background-color:#820014"><span class="main-color-text">red-9</span></div>
+          <div class="main-color-item" style="background-color:#5c0011"><span class="main-color-text">red-10</span></div>
+        </div>
+      </div>
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Volcano / 火山</div>
+          <span class="description">醒目、澎湃</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff2e8"><span class="main-color-text">volcano-1</span></div>
+          <div class="main-color-item" style="background-color:#ffd8bf"><span class="main-color-text">volcano-2</span></div>
+          <div class="main-color-item" style="background-color:#ffbb96"><span class="main-color-text">volcano-3</span></div>
+          <div class="main-color-item" style="background-color:#ff9c6e"><span class="main-color-text">volcano-4</span></div>
+          <div class="main-color-item" style="background-color:#ff7a45"><span class="main-color-text">volcano-5</span></div>
+          <div class="main-color-item" style="background-color:#fa541c"><span class="main-color-text">volcano-6</span></div>
+          <div class="main-color-item" style="background-color:#d4380d"><span class="main-color-text">volcano-7</span></div>
+          <div class="main-color-item" style="background-color:#ad2102"><span class="main-color-text">volcano-8</span></div>
+          <div class="main-color-item" style="background-color:#871400"><span class="main-color-text">volcano-9</span></div>
+          <div class="main-color-item" style="background-color:#610b00"><span class="main-color-text">volcano-10</span></div>
+        </div>
+      </div>
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Sunset Orange / 日暮</div>
+          <span class="description">温暖、欢快</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff7e6"><span class="main-color-text">orange-1</span></div>
+          <div class="main-color-item" style="background-color:#ffe7ba"><span class="main-color-text">orange-2</span></div>
+          <div class="main-color-item" style="background-color:#ffd591"><span class="main-color-text">orange-3</span></div>
+          <div class="main-color-item" style="background-color:#ffc069"><span class="main-color-text">orange-4</span></div>
+          <div class="main-color-item" style="background-color:#ffa940"><span class="main-color-text">orange-5</span></div>
+          <div class="main-color-item" style="background-color:#fa8c16"><span class="main-color-text">orange-6</span></div>
+          <div class="main-color-item" style="background-color:#d46b08"><span class="main-color-text">orange-7</span></div>
+          <div class="main-color-item" style="background-color:#ad4e00"><span class="main-color-text">orange-8</span></div>
+          <div class="main-color-item" style="background-color:#873800"><span class="main-color-text">orange-9</span></div>
+          <div class="main-color-item" style="background-color:#612500"><span class="main-color-text">orange-10</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="palette-row">
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Dust Red / 薄暮</div>
+          <span class="description">斗志、奔放</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff1f0"><span class="main-color-text">red-1</span></div>
+          <div class="main-color-item" style="background-color:#ffccc7"><span class="main-color-text">red-2</span></div>
+          <div class="main-color-item" style="background-color:#ffa39e"><span class="main-color-text">red-3</span></div>
+          <div class="main-color-item" style="background-color:#ff7875"><span class="main-color-text">red-4</span></div>
+          <div class="main-color-item" style="background-color:#ff4d4f"><span class="main-color-text">red-5</span></div>
+          <div class="main-color-item" style="background-color:#f5222d"><span class="main-color-text">red-6</span></div>
+          <div class="main-color-item" style="background-color:#cf1322"><span class="main-color-text">red-7</span></div>
+          <div class="main-color-item" style="background-color:#a8071a"><span class="main-color-text">red-8</span></div>
+          <div class="main-color-item" style="background-color:#820014"><span class="main-color-text">red-9</span></div>
+          <div class="main-color-item" style="background-color:#5c0011"><span class="main-color-text">red-10</span></div>
+        </div>
+      </div>
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Volcano / 火山</div>
+          <span class="description">醒目、澎湃</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff2e8"><span class="main-color-text">volcano-1</span></div>
+          <div class="main-color-item" style="background-color:#ffd8bf"><span class="main-color-text">volcano-2</span></div>
+          <div class="main-color-item" style="background-color:#ffbb96"><span class="main-color-text">volcano-3</span></div>
+          <div class="main-color-item" style="background-color:#ff9c6e"><span class="main-color-text">volcano-4</span></div>
+          <div class="main-color-item" style="background-color:#ff7a45"><span class="main-color-text">volcano-5</span></div>
+          <div class="main-color-item" style="background-color:#fa541c"><span class="main-color-text">volcano-6</span></div>
+          <div class="main-color-item" style="background-color:#d4380d"><span class="main-color-text">volcano-7</span></div>
+          <div class="main-color-item" style="background-color:#ad2102"><span class="main-color-text">volcano-8</span></div>
+          <div class="main-color-item" style="background-color:#871400"><span class="main-color-text">volcano-9</span></div>
+          <div class="main-color-item" style="background-color:#610b00"><span class="main-color-text">volcano-10</span></div>
+        </div>
+      </div>
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Sunset Orange / 日暮</div>
+          <span class="description">温暖、欢快</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff7e6"><span class="main-color-text">orange-1</span></div>
+          <div class="main-color-item" style="background-color:#ffe7ba"><span class="main-color-text">orange-2</span></div>
+          <div class="main-color-item" style="background-color:#ffd591"><span class="main-color-text">orange-3</span></div>
+          <div class="main-color-item" style="background-color:#ffc069"><span class="main-color-text">orange-4</span></div>
+          <div class="main-color-item" style="background-color:#ffa940"><span class="main-color-text">orange-5</span></div>
+          <div class="main-color-item" style="background-color:#fa8c16"><span class="main-color-text">orange-6</span></div>
+          <div class="main-color-item" style="background-color:#d46b08"><span class="main-color-text">orange-7</span></div>
+          <div class="main-color-item" style="background-color:#ad4e00"><span class="main-color-text">orange-8</span></div>
+          <div class="main-color-item" style="background-color:#873800"><span class="main-color-text">orange-9</span></div>
+          <div class="main-color-item" style="background-color:#612500"><span class="main-color-text">orange-10</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="palette-row">
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Dust Red / 薄暮</div>
+          <span class="description">斗志、奔放</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff1f0"><span class="main-color-text">red-1</span></div>
+          <div class="main-color-item" style="background-color:#ffccc7"><span class="main-color-text">red-2</span></div>
+          <div class="main-color-item" style="background-color:#ffa39e"><span class="main-color-text">red-3</span></div>
+          <div class="main-color-item" style="background-color:#ff7875"><span class="main-color-text">red-4</span></div>
+          <div class="main-color-item" style="background-color:#ff4d4f"><span class="main-color-text">red-5</span></div>
+          <div class="main-color-item" style="background-color:#f5222d"><span class="main-color-text">red-6</span></div>
+          <div class="main-color-item" style="background-color:#cf1322"><span class="main-color-text">red-7</span></div>
+          <div class="main-color-item" style="background-color:#a8071a"><span class="main-color-text">red-8</span></div>
+          <div class="main-color-item" style="background-color:#820014"><span class="main-color-text">red-9</span></div>
+          <div class="main-color-item" style="background-color:#5c0011"><span class="main-color-text">red-10</span></div>
+        </div>
+      </div>
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Volcano / 火山</div>
+          <span class="description">醒目、澎湃</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff2e8"><span class="main-color-text">volcano-1</span></div>
+          <div class="main-color-item" style="background-color:#ffd8bf"><span class="main-color-text">volcano-2</span></div>
+          <div class="main-color-item" style="background-color:#ffbb96"><span class="main-color-text">volcano-3</span></div>
+          <div class="main-color-item" style="background-color:#ff9c6e"><span class="main-color-text">volcano-4</span></div>
+          <div class="main-color-item" style="background-color:#ff7a45"><span class="main-color-text">volcano-5</span></div>
+          <div class="main-color-item" style="background-color:#fa541c"><span class="main-color-text">volcano-6</span></div>
+          <div class="main-color-item" style="background-color:#d4380d"><span class="main-color-text">volcano-7</span></div>
+          <div class="main-color-item" style="background-color:#ad2102"><span class="main-color-text">volcano-8</span></div>
+          <div class="main-color-item" style="background-color:#871400"><span class="main-color-text">volcano-9</span></div>
+          <div class="main-color-item" style="background-color:#610b00"><span class="main-color-text">volcano-10</span></div>
+        </div>
+      </div>
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Sunset Orange / 日暮</div>
+          <span class="description">温暖、欢快</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff7e6"><span class="main-color-text">orange-1</span></div>
+          <div class="main-color-item" style="background-color:#ffe7ba"><span class="main-color-text">orange-2</span></div>
+          <div class="main-color-item" style="background-color:#ffd591"><span class="main-color-text">orange-3</span></div>
+          <div class="main-color-item" style="background-color:#ffc069"><span class="main-color-text">orange-4</span></div>
+          <div class="main-color-item" style="background-color:#ffa940"><span class="main-color-text">orange-5</span></div>
+          <div class="main-color-item" style="background-color:#fa8c16"><span class="main-color-text">orange-6</span></div>
+          <div class="main-color-item" style="background-color:#d46b08"><span class="main-color-text">orange-7</span></div>
+          <div class="main-color-item" style="background-color:#ad4e00"><span class="main-color-text">orange-8</span></div>
+          <div class="main-color-item" style="background-color:#873800"><span class="main-color-text">orange-9</span></div>
+          <div class="main-color-item" style="background-color:#612500"><span class="main-color-text">orange-10</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="palette-row">
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Dust Red / 薄暮</div>
+          <span class="description">斗志、奔放</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff1f0"><span class="main-color-text">red-1</span></div>
+          <div class="main-color-item" style="background-color:#ffccc7"><span class="main-color-text">red-2</span></div>
+          <div class="main-color-item" style="background-color:#ffa39e"><span class="main-color-text">red-3</span></div>
+          <div class="main-color-item" style="background-color:#ff7875"><span class="main-color-text">red-4</span></div>
+          <div class="main-color-item" style="background-color:#ff4d4f"><span class="main-color-text">red-5</span></div>
+          <div class="main-color-item" style="background-color:#f5222d"><span class="main-color-text">red-6</span></div>
+          <div class="main-color-item" style="background-color:#cf1322"><span class="main-color-text">red-7</span></div>
+          <div class="main-color-item" style="background-color:#a8071a"><span class="main-color-text">red-8</span></div>
+          <div class="main-color-item" style="background-color:#820014"><span class="main-color-text">red-9</span></div>
+          <div class="main-color-item" style="background-color:#5c0011"><span class="main-color-text">red-10</span></div>
+        </div>
+      </div>
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Volcano / 火山</div>
+          <span class="description">醒目、澎湃</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff2e8"><span class="main-color-text">volcano-1</span></div>
+          <div class="main-color-item" style="background-color:#ffd8bf"><span class="main-color-text">volcano-2</span></div>
+          <div class="main-color-item" style="background-color:#ffbb96"><span class="main-color-text">volcano-3</span></div>
+          <div class="main-color-item" style="background-color:#ff9c6e"><span class="main-color-text">volcano-4</span></div>
+          <div class="main-color-item" style="background-color:#ff7a45"><span class="main-color-text">volcano-5</span></div>
+          <div class="main-color-item" style="background-color:#fa541c"><span class="main-color-text">volcano-6</span></div>
+          <div class="main-color-item" style="background-color:#d4380d"><span class="main-color-text">volcano-7</span></div>
+          <div class="main-color-item" style="background-color:#ad2102"><span class="main-color-text">volcano-8</span></div>
+          <div class="main-color-item" style="background-color:#871400"><span class="main-color-text">volcano-9</span></div>
+          <div class="main-color-item" style="background-color:#610b00"><span class="main-color-text">volcano-10</span></div>
+        </div>
+      </div>
+      <div class="palette">
+        <div class="title">
+          <div class="main-title">Sunset Orange / 日暮</div>
+          <span class="description">温暖、欢快</span>
+        </div>
+        <div class="main-color">
+          <div class="main-color-item" style="background-color:#fff7e6"><span class="main-color-text">orange-1</span></div>
+          <div class="main-color-item" style="background-color:#ffe7ba"><span class="main-color-text">orange-2</span></div>
+          <div class="main-color-item" style="background-color:#ffd591"><span class="main-color-text">orange-3</span></div>
+          <div class="main-color-item" style="background-color:#ffc069"><span class="main-color-text">orange-4</span></div>
+          <div class="main-color-item" style="background-color:#ffa940"><span class="main-color-text">orange-5</span></div>
+          <div class="main-color-item" style="background-color:#fa8c16"><span class="main-color-text">orange-6</span></div>
+          <div class="main-color-item" style="background-color:#d46b08"><span class="main-color-text">orange-7</span></div>
+          <div class="main-color-item" style="background-color:#ad4e00"><span class="main-color-text">orange-8</span></div>
+          <div class="main-color-item" style="background-color:#873800"><span class="main-color-text">orange-9</span></div>
+          <div class="main-color-item" style="background-color:#612500"><span class="main-color-text">orange-10</span></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +233,60 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.warp{
+  width: 100%;
+  height: 2300px;
+  & .palette-row{
+    width: 100%;
+    height: 536px;
+    margin-bottom: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    & .palette{
+      width: 25%;
+      height: 536px;
+      margin-right: 5%;
+      margin-left: 5%;
+      & .title{
+        margin: 0 0 24px;
+        color: #5c6b77;
+        font-weight: 500;
+        height: 70px;
+        font-size: 22px;
+        text-align: center;
+        & .main-title{
+          height: 50px;
+          line-height: 50px;
+        }
+        // text-transform: capitalize;
+        & .description{
+          display: block;
+          color: #777;
+          font-weight: lighter;
+          font-size: 14px;
+        }
+      }
+      & .main-color{
+        width: 100%;
+        height: 440px;
+        & .main-color-item:hover{
+          border-radius: 0px 5px 5px 0px;
+          width: 102%;
+        }
+        & .main-color-item{
+          width:100%;
+          height: 44px;
+          & .main-color-text{
+            font-size: 14px;
+            line-height: 44px;
+            padding-left:20px ;
+            color: #aaaaaa;
+          }
+        }
+      }
+    }
+  }
+}
 </style>

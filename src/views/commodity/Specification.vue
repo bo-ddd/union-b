f464b455a354efa5296418189fb184b62a3fc1ea
hyx1958895296 <1958895296@qq.com>
2021-12-16 16:42:52 +0800

    商品规格页面中删除功能已完成

--- src/views/commodity/Specification.vue
+++ src/views/commodity/Specification.vue
@@ -48,29 +48,38 @@
           show-overflow-tooltip
           align="center"
         >
-          <template>
-            <el-button type="primary" i class="el-icon-edit cell1"></el-button>
+          <template slot-scope="scope">
             <el-button
               type="primary"
               i
-              class="el-icon-delete cell2"
+              class="el-icon-edit cell1"
+              @click="editListData"
             ></el-button>
+            <el-button
+              i class="el-icon-delete cell2"
+              @click.native.prevent="deleteRow(scope.$index, tableData)"
+              type="text"             
+            >
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
       <div class="footer">
         <div class="footer_left">
           <el-button type="primary" plain size="small">保存排序</el-button>
-          <el-button plain class="batch_del_btn" size="small"
-            @click="batchesDelete">批量删除</el-button
+          <el-button
+            plain
+            class="batch_del_btn"
+            size="small"
+            @click="batchesDelete"
+            >批量删除</el-button
           >
         </div>
         <div class="footer_right">
           <div class="block">
-            <div>每页显示</div>
             <el-pagination
               :current-page="currentPage4"
-              :page-sizes="[6]"
+              :page-sizes="[6, 10, 15, 20, 30, 50, 100]"
               layout="sizes"
             >
             </el-pagination>
@@ -80,7 +89,6 @@
             <el-pagination
               @size-change="handleSizeChange"
               @current-change="handleCurrentChange"
-              :current-page="currentPage4"
               :page-size="100"
               :total="6"
               layout="total, prev, pager, next"
@@ -94,14 +102,15 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   data() {
     return {
       input: 10,
-      currentPage1: 5,
-      currentPage2: 5,
-      currentPage3: 5,
       currentPage4: 4,
+      pagination: true,
+      pageNum: 10,
+      pageSize: 1,
       tableData: [
         {
           id: "1",
@@ -150,6 +159,7 @@ export default {
     };
   },
   methods: {
+    ...mapActions(["getSpecificationList"]),
     toggleSelection(rows) {
       if (rows) {
         rows.forEach((row) => {
@@ -168,12 +178,30 @@ export default {
     handleCurrentChange(val) {
       console.log(`当前页: ${val}`);
     },
+    //添加规格
     addspecification() {
       console.log("添加成功");
     },
-    batchesDelete(){
-      console.log('批量删除成功');
-    }
+    //批量删除
+    batchesDelete() {
+      console.log("批量删除成功");
+    },
+    //单个修改
+    editListData() {
+      console.log("单个修改成功");
+    },
+    //单个删除
+    deleteRow(index, rows) {
+      rows.splice(index, 1);
+    },
+  },
+  async created() {
+    let res = await this.getSpecificationList({
+      pagination: this.pagination,
+      pageNum: this.pageNum,
+      pageSize: this.pageSize,
+    });
+    console.log(res);
   },
 };
 </script>
@@ -184,8 +212,7 @@ export default {
   border: 1px solid #d4dde2;
   overflow: hidden;
   & .wrap_interior {
-    // min-width: 1200px;
-    overflow-y:auto;
+    overflow-y: auto;
     height: 85vh;
   }
   & .tit {
@@ -220,7 +247,7 @@ export default {
         color: #999999;
         font-size: 14px;
         & .el-pagination {
-          padding: 5px 5px; 
+          padding: 5px 5px;
         }
       }
     }
@@ -245,5 +272,6 @@ export default {
   background-color: #ff8b53;
   padding: 10px 10px;
   border: none;
+  color: #ffffff;
 }
 </style>

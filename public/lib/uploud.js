b9972ef3f70bdba33885a334da490ea9e31bc6e7
xiaozhi <2282856721@qq.com>
2021-12-22 09:20:08 +0800

    pull

--- public/lib/uploud.js
+++ public/lib/uploud.js
@@ -1,7 +1,10 @@
 export default  () =>{
     let formData = new FormData();
-    formData.append('file',this.$refs.file.files[0]);
-    formData.append('type',2) 
-    let res  =  this.uploadImage(formData);
-    return res
+    let file = document.getElementById('file').files[0];  //this.$refs.file.files[0] 获取到上传的文件
+    console.log(file);
+    formData.append('file',document.getElementById('file').files[0])  //把获取到的文件append到formData里面
+    formData.append('type',1)                                         //把type append到formData里面
+    console.log(formData.get('file'));    //拿到formData里面的file并打印
+    console.log(formData.get('type'));    //拿到formData里面的type并打印
+    return formData
 }

7dc1d71b14b97c378644c7179277e03931db397d
bigC <814151789@qq.com>
2021-12-26 23:27:19 +0800

    update code

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack')
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const productionGzipExtensions = ['js', 'css']
 // const isProduction = process.env.NODE_ENV === 'production'
@@ -21,9 +20,6 @@ module.exports = {
     productionSourceMap: false,
     configureWebpack:{
         plugins: [
-          // Ignore all locale files of moment.js
-          new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-          
           // 配置compression-webpack-plugin压缩
           new CompressionWebpackPlugin({
             algorithm: 'gzip',
@@ -31,10 +27,6 @@ module.exports = {
             threshold: 10240,
             minRatio: 0.8
           }),
-          new webpack.optimize.LimitChunkCountPlugin({
-            maxChunks: 5, 
-            minChunkSize: 100
-          })
         ]
       },
 }

c84d4d04a77e651a592cd367088e99a639407e16
gtf1230 <877710320@qq.com>
2021-12-27 20:19:48 +0800

    更新 jiekou

--- src/store/index.js
+++ src/store/index.js
@@ -23,7 +23,7 @@ export default new Vuex.Store({
         needReCategoryList: true,
         categorylist: {},
         needGetTradeData: true,
-        tradeData: {}
+        tradeData: {},
         routes: getRoutes()
     },
     mutations: {

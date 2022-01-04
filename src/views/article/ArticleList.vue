ddaba373966e3d0e68b25418ebad4c46f4890fec
xx2747913644 <2747913644@qq.com>
2022-01-04 16:51:06 +0800

    一个

--- src/views/article/ArticleList.vue
+++ src/views/article/ArticleList.vue
@@ -7,35 +7,47 @@
       <el-table-column prop="articleTitle" label="文章标题" width="230">
       </el-table-column>
       <el-table-column prop="articleContent" label="文章内容">
+       <template slot-scope="scope">
+        <div v-html="scope.row.articleContent"></div>
+       </template>
       </el-table-column>
       <el-table-column prop="updatedAt" label="更改日期" width="230">
       </el-table-column>
       <el-table-column fixed="right" label="操作" width="230">
         <template slot-scope="scope">
-          <el-button class="button" @click="remove(scope.row)" type="text">移除</el-button>
-          <el-button class="button" @click="find(scope.row)" type="text">查看</el-button>
-          <el-button class="button" @click="update(scope.row)" type="text">更改</el-button>
+          <el-button class="button" @click="remove(scope.row)" type="text"
+            >移除</el-button
+          >
+          <el-button class="button" @click="find(scope.row)" type="text"
+            >查看</el-button
+          >
+          <!-- <el-button class="button" @click="update(scope.row)" type="text">更改</el-button> -->
+          <el-button type="text" @click="update(scope.row)">更改</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-dialog  :visible.sync="dialogFormVisible">
+
+    <el-dialog :visible.sync="dialogFormVisible">
       <el-form :model="form">
-        <el-form-item label="文章内容" :label-width="formLabelWidth">
-          <el-input v-model="form.name" autocomplete="off"></el-input>
-        </el-form-item>
         <el-form-item label="文章标题" :label-width="formLabelWidth">
-          <el-input v-model="form.title" autocomplete="off"></el-input>
+          <el-input v-model="articleTitle" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="文字内容" :label-width="formLabelWidth">
+        </el-form-item>
+            <div ref="editor" @input="change"></div>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="modify">确 定</el-button>
+        <el-button type="primary" @click="modify"
+          >确 定</el-button
+        >
       </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
+import E from "wangeditor";
 import { mapActions } from "vuex";
 import { getTime } from "../../assets/until/until";
 export default {
@@ -45,12 +57,13 @@ export default {
       tableData: [],
       dialogTableVisible: false,
       dialogFormVisible: false,
-      form : {
-        name : '',
-        title : '',
-      }
+       articleContent: null,
+       cont:0,
+       articleTitle:"",
+      //  articleContent:""
     };
   },
+  props:['value'],
   created() {
     this.list();
   },
@@ -79,21 +92,43 @@ export default {
         },
       });
     },
+ 
     async update(a) {
+      console.log(a);
+      this.articleTitle=a.articleTitle;
+      this.articleContent=a.articleContent
       this.id = a.id
       this.dialogFormVisible = true;
+      this.initEditor();
     },
-    
-    async modify(){
-      this.dialogFormVisible = false;
+    initEditor() {
+      this.cont++;
+      if(this.cont!=1) return;
+     
+      this.$nextTick(() => {
+        const editor = new E(this.$refs.editor);
+        editor.config.onchange = (html) => {
+          // 第二步，监控变化，同步更新到 textarea
+          // $text1.val(html);
+          this.articleContent = html;
+        };
+        editor.create();
+        editor.txt.html(this.value) 
+      });
+    },
+   change(e){
+      this.$$emit('input',e.target.innerHTML)
+    },
+    async modify() {
+     this.dialogFormVisible = false;
       let res = await this.updateArticle({
-        id : this.id,
-        articleTitle : this.form.title,
-        articleContent : this.form.name
+        id: this.id,
+        articleContent: this.articleContent,
+        articleTitle: this.articleTitle,
       });
       console.log(res);
       this.list();
-    }
+    },
   },
 };
 </script>
@@ -101,5 +136,8 @@ export default {
 <style>
 .wrap {
   padding: 20px;
+}
+.el-dialog__body{
+  padding: 0px 20px;
 }
 </style>
